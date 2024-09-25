0c1f86924fc673ae537ec3741e28fa2e9ebdf141
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-09-24 21:35:05 -0300

    feed

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,12 +1,32 @@
+"use client"
 import Post from "@/components/post/Post";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Feed() {
-return (
+    const [posts, setPosts] = useState([]);
+
+    const buscarPosts = async () => {
+        const resposta = (await axios.get("http://localhost:8088/postagem")).data;
+        setPosts(resposta);
+    }
+
+    useEffect(() => {
+        buscarPosts();
+    })
+
+    return (
+
+
     <main className="grid justify-center font-poppins">
         <div className="text-center">
         <p className="text-3xl my-10 font-bold">Feed</p>
         </div>
-      <Post />
+        {posts.map((post: any) => (
+        <Post
+            {...post}
+        />
+        ))}
     </main>
     );
 }
--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,22 +1,29 @@
 import { FaWhatsappSquare } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
-export default function Post() {
+type typePost = {
+    usuario: string;
+    imagem: string;
+    legenda: string;
+    criacao: string;
+};
+
+export default function Post({usuario, imagem, legenda, criacao}: typePost) {
     return (
         <section className="border border-azul rounded mb-4">
             <div className="p-4">
                 <a className="text-lg">Publicado por: </a>
-                <a className="font-bold text-lg">Gabriel</a>
-                <p className="text-lg">Meme muito bom 2024 atualizado</p>
+                <a className="font-bold text-lg">{usuario}</a>
+                <p className="text-lg">{legenda}</p>
             </div>
             <div className="px-16 flex">
-                <img className="w-96" src="https://brunopredolin.com.br/wp-content/uploads/2023/01/la-eele-meme-mascote-copa-qatar.jpg" alt="Imagem do post" />
+                <img className="w-96" src={imagem} alt="Imagem do post" />
                 <div className="grid ml-10">
                     <button><FaWhatsappSquare size={40} /></button>
                     <button><MdEmail size={40} /></button>
                 </div>
             </div>
-            <p className="text-sm p-4">19:35 24/09/2024</p>
+            <p className="text-sm p-4">{criacao}</p>
         </section>
     );
 }
